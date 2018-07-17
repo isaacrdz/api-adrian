f21d08e55d7ddd8de4ed71e556bdab30f3241de7
isaacrdz <zack_cpp@hotmail.com>
2018-07-17 18:47:33 -0500

    test

--- routes/api/users.js
+++ routes/api/users.js
@@ -93,7 +93,7 @@ router.post("/login", (req, res) => {
           (err, token) => {
             res.json({
               success: true,
-              token: "Beared " + token
+              token: "Bearer " + token
             });
           }
         );
